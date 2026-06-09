c1242a5f7030ceff103a07cf2f9abfbb7d89e822
chebaane1-NINGEN <hr.chebaane@gmail.com>
2026-06-09 17:37:28 +0100

    fix static file

--- skands/skands/wsgi.py
+++ skands/skands/wsgi.py
@@ -8,10 +8,20 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skands.settings')
+
+# Cela s'exécute une seule fois au démarrage de la fonction Serverless
+if os.environ.get('VERCEL') == '1':
+    from django.core.management import call_command
+    try:
+        print("Vercel détecté : Lancement automatique de collectstatic...")
+        call_command('collectstatic', '--noinput', '--clear')
+        print("Collectstatic terminé avec succès !")
+    except Exception as e:
+        print(f"Erreur lors du collectstatic automatique : {e}", file=sys.stderr)
 
 application = get_wsgi_application()
 

81af2869520800b37986f207814eadca80be90e4
chebaane1-NINGEN <hr.chebaane@gmail.com>
2026-06-10 23:43:07 +0100

    sz

--- skands/build.sh
+++ skands/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-set -e
 
+# 1. Supprimer l'ancien dossier pour repartir sur une base propre
+rm -rf staticfiles/
+
+# 2. Lancer le collectstatic en forçant le nettoyage des anciens fichiers
 python manage.py collectstatic --noinput --clear
+
+# 3. Appliquer les migrations
 python manage.py migrate --noinput
